1ce80ae1869bfd7236f6f073c676b6bb593f16fc
shoya kusunose <shoya2020recruit@gmail.com>
2020-12-03 17:21:12 +0900

    update fetch_MAC.py

--- src/mac_address/kusunose/fetch_MAC.py
+++ src/mac_address/kusunose/fetch_MAC.py
@@ -1,9 +1,25 @@
 import re
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("htohu-881c2-firebase-adminsdk-m6g1v-607ecca66a.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+docs = db.collection('members').stream()
+data = []
+for doc in docs:
+    data.append(doc.to_dict()['addr'])
+
 
 with open('caughts', 'r') as f:
     reads = f.read()
 
-with open('macs', 'w') as f
+scan = []
 for i  in reads.strip().split('\n'):
-    print(re.match('(.*)\((.*)\) at (.*) on (.*)', i).groups()[2])
+    scan.append(re.match('(.*)\((.*)\) at (.*) on (.*)', i).groups()[2])
     
+for i in scan:
+    if i in data:
+        print(f'{i} is in the room!')

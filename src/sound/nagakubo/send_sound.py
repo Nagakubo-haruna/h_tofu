6a535475c03b47c3334ab3c37ac0d33a1e4b629d
Nagakubo-haruna <65169004+Nagakubo-haruna@users.noreply.github.com>
2020-12-04 09:37:34 +0900

    Add files via upload

---
+++ src/sound/nagakubo/send_sound.py
@@ -0,0 +1,19 @@
+import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+#Cloud Firestore SDK初期化
+cred = credentials.Certificate('htohu-881c2-firebase-adminsdk-m6g1v-607ecca66a.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+#タイムスタンプ取得
+now= datetime.datetime.now()
+#データ''追加
+doc_ref = db.collection(u'sound').document(u'ID')
+doc_ref.set({
+    u'volume': 1,
+    u'time': u'%Y_%H:%M:%S'.format(now)
+})

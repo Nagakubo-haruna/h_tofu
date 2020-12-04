b0de45573fce25d98eb48c289ee61c88ca681dc2
Nagakubo-haruna <a16.8dtj@g.chuo-u.ac.jp>
2020-12-04 18:50:12 +0900

    catch&send deta

--- src/sound/nagakubo/send_sound.py
+++ src/sound/nagakubo/send_sound.py
@@ -1,19 +1,58 @@
+
+import math
+import sys
+import time
+from grove.adc import ADC　
+ 
 import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-#Cloud Firestore SDK初期化
-cred = credentials.Certificate('htohu-881c2-firebase-adminsdk-m6g1v-607ecca66a.json')
-firebase_admin.initialize_app(cred)
+#音読み込み（sound sensor）
+class GroveSoundSensor:
+     
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+ 
+    @property
+    def sound(self):
+        value = self.adc.read(self.channel)
+        return value
+ 
+Grove = GroveSoundSensor
+ 
+ 
+def main():
+    #Cloud Firestore SDK初期化
+    cred = credentials.Certificate('htohu-881c2-firebase-adminsdk-m6g1v-607ecca66a.json')
+    firebase_admin.initialize_app(cred)
+    
+    db = firestore.client()
+    #センサーからデータ取得
+    if len(sys.argv) < 2:
+        print('Usage: {} adc_channel'.format(sys.argv[0]))
+        sys.exit(1)
+ 
+    sensor = GroveSoundSensor(int(sys.argv[1]))
+ 
+    print('Detecting sound...')
+    while True:
+         #タイムスタンプ取得
+        now= datetime.datetime.now()
+        #データ''追加
+        doc_ref = db.collection(u'sound').document()
+        doc_ref.set({
+            u'volume': sensor.sound,
+            u'time': str(now)
+        })
 
-db = firestore.client()
+        #print('Sound value: {0}'.format(sensor.sound))　#画面出力
+        time.sleep(1) #1秒間隔で取得
+ 
 
-#タイムスタンプ取得
-now= datetime.datetime.now()
-#データ''追加
-doc_ref = db.collection(u'sound').document()
-doc_ref.set({
-    u'volume': 1,
-    u'time': str(now)
-})
+if __name__ == '__main__':
+    main()
+
+

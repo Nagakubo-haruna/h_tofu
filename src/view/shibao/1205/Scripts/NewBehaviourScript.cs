358be5ddbb7c2c75e46442d78d859d129d788df2
Akinari-S <akinari.shibao@gmail.com>
2020-12-05 21:59:54 +0900

    update sample2.jslib
    sample2.jslib
    JSTest.cs

---
+++ src/view/shibao/1205/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+		this.gameObject.transform.position = new Vector3 (Mathf.Sin (Time.time) * 2.0f, pos.y, pos.z);	
+    }
+}
